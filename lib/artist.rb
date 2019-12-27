99fbeb02e81fa1d7561f46d560ff9e3d69f0f1ad
Nick Solonyy <nicksolony@gmail.com>
2019-12-27 03:23:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,28 +3,25 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
+  extend Memorable
+  extend Findable
+  include Paramable
 
   @@artists = []
 
-  def initialize
-    @@artists << self
+def initialize
+#    self.class.all << self
+  super
     @songs = []
-  end
+end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+
+# def self.find_by_name(name)
+#    @@songs.detect{|a| a.name == name}
+#  end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -36,7 +33,5 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+ 
 end
---
+++ lib/concerns/paramable.rb
@@ -0,0 +1,11 @@
+module Paramable
+
+ def to_param
+    name.downcase.gsub(' ', '-')
+  end
+  
+   def initialize
+      self.class.all << self
+  end
+
+end
